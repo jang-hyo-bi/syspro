4b21a990071101e609641c63384274471314292f
jang-hyo-bi <144859897+jang-hyo-bi@users.noreply.github.com>
2023-12-13 23:53:42 +0900

    Create main.c

---
+++ chap11/prob9/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <signal.h>
+
+void alarmHandler();
+
+int main() {
+   struct sigaction sa;
+   sa.sa_handler = alarmHandler;
+   sigemptyset(&sa.sa_mask);
+   sa.sa_flags = 0;
+
+   sigaction(SIGALRM, &sa, NULL);
+
+   alarm(5);
+   short i = 0;
+   while (1) {
+      sleep(1);
+      i++;
+      printf("%d second\n", i);
+   }
+   printf("end\n");
+}
+
+void alarmHandler(int signo) {
+   printf("Wake up\n");
+   exit(0);
+}
